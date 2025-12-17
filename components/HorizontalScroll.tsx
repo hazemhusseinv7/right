702fdfb14cf718f59ca3c3c7745f8ee9fd1dffac
Hazem <hazemhusseinv7@gmail.com>
2025-12-18 01:58:00 +0200

    Fixed scroll bug

--- components/HorizontalScroll.tsx
+++ components/HorizontalScroll.tsx
@@ -46,13 +46,13 @@ export default function HorizontalScroll({
 
   return (
     <ReactLenis root>
-      <section ref={sectionRef} className="relative h-[500vh]">
+      <section ref={sectionRef} className="relative h-[200vh] lg:h-[300vh]">
         <ul ref={ulRef} className="sticky top-0 flex">
           <li className="bg-primary-green flex h-screen w-screen min-w-screen flex-col items-center justify-start overflow-hidden pt-20">
             <h2 className="text-primary-blue relative bottom-5 inline-block text-[10vw] font-semibold">
               {aboutUs?.ourVision.title}
             </h2>
-            <p className="text-primary-blue relative bottom-5 inline-block max-w-5xl px-8 text-lg lg:text-2xl">
+            <p className="text-primary-blue relative bottom-5 inline-block max-w-5xl px-8 text-center text-lg lg:text-2xl">
               {aboutUs?.ourVision.description}
             </p>
             <Image
@@ -63,11 +63,11 @@ export default function HorizontalScroll({
               alt={aboutUs?.ourVision.title || "image"}
             />
           </li>
-          <li className="bg-primary-blue flex h-screen w-screen min-w-screen flex-col items-center justify-start overflow-hidden pt-20">
+          <li className="bg-primary-blue hidden h-screen w-screen min-w-screen flex-col items-center justify-start overflow-hidden pt-20 lg:flex">
             <h2 className="text-primary-green relative bottom-5 inline-block text-[10vw] font-semibold">
               {aboutUs?.ourMission.title}
             </h2>
-            <p className="text-primary-green relative bottom-5 inline-block max-w-5xl px-8 text-lg lg:text-2xl">
+            <p className="text-primary-green relative bottom-5 inline-block max-w-5xl px-8 text-center text-lg lg:text-2xl">
               {aboutUs?.ourMission.description}
             </p>
             <Image
@@ -80,6 +80,21 @@ export default function HorizontalScroll({
           </li>
         </ul>
       </section>
+      <div className="bg-primary-blue flex h-screen w-screen min-w-screen flex-col items-center justify-start overflow-hidden pt-20 lg:hidden">
+        <h2 className="text-primary-green relative bottom-5 inline-block text-[10vw] font-semibold">
+          {aboutUs?.ourMission.title}
+        </h2>
+        <p className="text-primary-green relative bottom-5 inline-block max-w-5xl px-8 text-center text-lg lg:text-2xl">
+          {aboutUs?.ourMission.description}
+        </p>
+        <Image
+          src={urlFor(aboutUs?.ourMission.image).url()}
+          className="absolute bottom-0 w-[380px] 2xl:w-[750px]"
+          width={500}
+          height={500}
+          alt={aboutUs?.ourMission.title || "image"}
+        />
+      </div>
     </ReactLenis>
   );
 }
